5ace53b4c9375cafe2e7add33e52e7dfc20b7eba
Fabiano Terada <65690930+fabianoterada@users.noreply.github.com>
2025-06-21 16:38:56 -0300

    Update Lista exercicios2.js

--- Lista Exec2/Lista exercicios2.js
+++ Lista Exec2/Lista exercicios2.js
@@ -55,3 +55,91 @@ console.log('Você acertou!!! O número escolhido foi o  ', sugestao , 'você pr
 
 jogoAdivinhacao();
 */
+
+// EXERCÍCIO 3 -  Palavras Únicas
+// Dada uma string (ex.: "olá olá mundo mundo"), use if/else e for para extrair 
+// todas as palavras únicas e exibi-las em um array.
+
+/*
+const frase = "olá olá mundo tudo bem bem e ai ai"
+
+
+// Quebrando a frase em palavras:
+const palavras = frase.split(" ")
+
+console.log(palavras)
+
+
+const unicas = []; // Array para guardar palavras únicas
+
+for (let i = 0; i < palavras.length; i++) {
+    let palavraAtual = palavras[i]
+    if (!unica.includes(palavraAtual)){
+        unica.push(palavraAtual)
+    }
+}  
+console.log("Palavras únicas:", unica)
+*/
+
+// EXERCÍCIO 4 -  Fatorial Recursivo
+//Implemente function fatorial(n) de forma recursiva; 
+// trate n < 0 lançando um Error, e n === 0 retornando 1.
+/*
+function fatorial(n) {
+  if (n < 0) {
+     throw new Error("Fatorial não definido para n < 0")
+  }
+    
+  if (n === 0) {
+    return 1
+  }
+
+  return n * fatorial(n - 1);
+}
+
+
+console.log(fatorial(4)); //24
+*/
+
+// EXERCÍCIO 5 - Debounce Crie function debounce(fn, delay) que receba uma função fn e um delay em ms, 
+// retornando uma nova função que só executa fn se não for chamada novamente dentro do intervalo.
+/*
+function debounce(fn, delay) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn.apply(this, args), delay);
+  };
+}
+
+// Teste:
+ const log = debounce(() => console.log('Executando Teste!'), 1000);
+log();
+log(); 
+*/
+
+// // EXERCÍCIO 6 -  Memoization 
+// Implemente function memoize(fn) que armazene em cache chamadas 
+// anteriores de fn (por argumentos), 
+// retornando resultados instantâneos em repetidas invocações.
+/*
+function memoize(fn) {
+    const cache = new Map();
+
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        const result = fn.apply(this, args);
+        cache.set(key, result);
+        return result;
+    }
+}
+
+// Testando o armazenamento:
+const slowFn = (n) => n / 2;
+const memoizedFn = memoize(slowFn);
+console.log(memoizedFn(16)); // Calcula
+console.log(memoizedFn(16)); // Do cache
+*/
